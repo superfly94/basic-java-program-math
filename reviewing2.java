239e62cc3dcd8bcea9995ee882366612e29fbda2
superfly94 <34755641+superfly94@users.noreply.github.com>
2018-03-08 17:06:53 -0600

    This is a bar bones basic math

---
+++ reviewing2.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+
+public class reviewing2 {
+public static void main(String args[]) {
+	Scanner keyboard=new Scanner(System.in);
+	
+		
+	// create a math thin with two inputs 
+int user1, user2, sum;
+
+System.out.print("Please enter a number you wish to multiply  ");
+user1=keyboard.nextInt();
+System.out.print("Please enter another number you wish to multiply  ");
+user2=keyboard.nextInt();
+sum=user1*user2;
+
+System.out.print("You entered "+user1+ " with "+user2+" which is "+ sum);
+
+}
+}		
+			
+	
+
+
+
+
+		
+	
+	
+	
+
+
+
+
+
+
+
+
+	
+	
+
+
